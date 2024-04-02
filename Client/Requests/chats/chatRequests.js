731d25dc8d0a17c04a24a036ab4f77bf6b8e63fb
mark <markgolah69@gmail.com>
2024-04-02 17:14:20 +0100

    commit

--- Client/Requests/chats/chatRequests.js
+++ Client/Requests/chats/chatRequests.js
@@ -5,6 +5,7 @@ const API = axios.create({baseURL: Base_Url})
 
 const chatRequests = {
     createChat : (recieverId , senderId)=>API.post(`/create/${recieverId}/${senderId}`, {withCredentials: true}),
+    getAllChat : () => API.get('/', {withCredentials: true})
 }
 
 export default chatRequests;
